17a45a8afb23ba50b85c90c2d37f3b2866d7813f
zzburning <zzburning@gmail.com>
2016-04-01 13:22:30 +0800

    add git difftool/mergetool; add setup_env.sh script

---
+++ cfgfiles/setup_env.sh
@@ -0,0 +1,6 @@
+   apt-get install vim tmux zsh git tree
+   chsh -s /bin/zsh
+   sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   apt-get install ack-grep ctags cscope python-pip
+   pip install httpie cheat
