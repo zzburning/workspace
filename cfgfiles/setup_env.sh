68f5b0bd0d84dc2ae87b263c987ea954541d3616
zzburning <zzburning@gmail.com>
2016-04-14 17:14:37 +0800

    update cmd.log; setup_env.sh

--- cfgfiles/setup_env.sh
+++ cfgfiles/setup_env.sh
@@ -1,16 +1,18 @@
 apt-get install vim tmux zsh git tree ack-grep ctags cscope python-pip
-
 pip install httpie cheat
 chsh -s /bin/zsh
+
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 git clone https://github.com/zzburning/workspace.git
-# copy cfgfile to user home dir
 
-# get ycm & compile
+# # --copy cfgfile/{.tmux.conf, .vimrc, .vimr.bundles, .zshrc, .gitconfig, .gitignore_global} to user home dir
+# # --install vim plugins
+# vim -c "PluginInstall"
+# # --compile ycm 
 # cd ~/.vim/bundle/YouCompleteMe
 # ./install.py --clang-completer --system-libclang --system-boost
-# 
+# # --or
 # cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 # cmake --build . --target ycm_support_libs --config Release
 
